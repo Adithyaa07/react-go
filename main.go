b9b7873bf02b306dbd4660943559c1e3492c68bd
AdithyaRaj <113771824+Adithyaa07@users.noreply.github.com>
2024-10-07 19:02:27 +0530

    Completed

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -23,7 +24,6 @@ type Todo struct {
 var collection *mongo.Collection
 
 func main() {
-	fmt.Println("Hello Adithya")
 
 	if os.Getenv("ENV") != "production" {
 		err := godotenv.Load(".env")
@@ -52,10 +52,10 @@ func main() {
 	collection = client.Database("golang_db").Collection("todos")
 	app := fiber.New()
 
-	// app.Use(cors.New(cors.Config{
-	// 	AllowOrigins: "http://localhost:5173",
-	// 	AllowHeaders: "Origin, Content-Type, Accept ",
-	// }))
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173",
+		AllowHeaders: "Origin, Content-Type, Accept ",
+	}))
 
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodo)
